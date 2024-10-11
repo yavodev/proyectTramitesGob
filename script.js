b4dbd7d32a67bf2b0ce43838333aa110a4b75b2c
Yerson Vargas <yersonvargas18@gmail.com>
2024-10-11 13:10:40 -0500

    agregando archivos del proyecto

---
+++ script.js
@@ -0,0 +1,5 @@
+// Ejemplo simple de validación
+document.querySelector('form').addEventListener('submit', function(e) {
+    alert('Tu solicitud ha sido enviada correctamente.');
+    e.preventDefault();
+});
